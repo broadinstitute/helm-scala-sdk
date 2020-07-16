c8ce3c4fe585101b0601913c4418f6fddc99ce3b
Kaan Yuksel <kyuksel@broadinstitute.org>
2020-07-16 13:09:03 -0400

    Fix typo and add comment

--- src/test/scala/org/broadinstitute/dsp/HelmManualTest.scala
+++ src/test/scala/org/broadinstitute/dsp/HelmManualTest.scala
@@ -6,9 +6,12 @@ import cats.effect.IO
  * How to run this:
  * sbt -Djna.library.path=/Users/qi/workspace/helm-scala-sdk/helm-go-lib test:console
  *
- * Once inside sbt shell, call the function directly, eg: org.broadinstitute.dsp.HelmMannualTest.listHelm()
+ * Once inside sbt shell, call the function directly, eg: org.broadinstitute.dsp.HelmManualTest.listHelm()
+ *
+ * KubeApiServer and KubeToken can be retrieved via kubectl as described below:
+ * https://kubernetes.io/docs/tasks/administer-cluster/access-cluster-api/#without-kubectl-proxy
  */
-object HelmMannualTest extends HelmScalaSdkTestSuite {
+object HelmManualTest extends HelmScalaSdkTestSuite {
   val helmClient = new Helm[IO](blocker, semaphore)
   val authContext = AuthContext(
     Namespace("galaxy"),
@@ -18,9 +21,9 @@ object HelmMannualTest extends HelmScalaSdkTestSuite {
 
   def install(): Unit = {
     helmClient.install(
-      "qi-galaxy-rls-3",
+      "ky-galaxy-rls-0716-1",
       "galaxyproject/galaxy",
-      "/Users/qi/workspace/galaxy-cvmfs-csi-helm/galaxy-cvmfs-csi/values.yaml"
+      "/Users/kyuksel/gke_experiment/galaxy-cvmfs-csi-helm/galaxy-cvmfs-csi/values.yaml"
     ).run(authContext)
       .unsafeRunSync()
   }
