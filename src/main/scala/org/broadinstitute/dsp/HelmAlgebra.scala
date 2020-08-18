7482a51f07d336354363d55fa6e15e3839899dac
Rob Title <rtitle@broadinstitute.org>
2020-08-18 15:31:42 -0400

    Add some model documentation

--- src/main/scala/org/broadinstitute/dsp/HelmAlgebra.scala
+++ src/main/scala/org/broadinstitute/dsp/HelmAlgebra.scala
@@ -5,14 +5,9 @@ import cats.data.Kleisli
 import scala.util.control.NoStackTrace
 
 trait HelmAlgebra[F[_]] {
-  // 'values' can be in comma-separated key/value pair format as in CLI --set (e.g. "key1=v1,key2.key3=v2")
-  // More complex use cases are also supported as depicted below
-  // https://helm.sh/docs/intro/using_helm/#the-format-and-limitations-of---set
   def installChart(
     release: Release,
     chart: Chart,
-    // TODO: Look into better representing values (e.g. Map[String, String])
-    // once we know what fulfils our use case for overriding chart values
     values: Values
   ): Kleisli[F, AuthContext, Unit]
 
@@ -32,8 +27,19 @@ final case class AuthContext(
 )
 
 final case class Namespace(asString: String) extends AnyVal
-final case class KubeToken(asString: String) extends AnyVal
-final case class KubeApiServer(asString: String) extends AnyVal
 final case class Release(asString: String) extends AnyVal
 final case class Chart(asString: String) extends AnyVal
+
+// If running on GKE, this can be a Google access token as long as it
+// includes GKE scopes.
+final case class KubeToken(asString: String) extends AnyVal
+
+// Note: this value should be a full URL (e.g. https://<ip-address>)
+final case class KubeApiServer(asString: String) extends AnyVal
+
+// Values can be in comma-separated key/value pair format as in CLI --set (e.g. "key1=v1,key2.key3=v2")
+// More complex use cases are also supported as depicted below
+// https://helm.sh/docs/intro/using_helm/#the-format-and-limitations-of---set
+// TODO: Look into better representing values (e.g. Map[String, String])
+// once we know what fulfils our use case for overriding chart values
 final case class Values(asString: String) extends AnyVal
