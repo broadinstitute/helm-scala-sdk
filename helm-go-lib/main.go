e1ef57171263dfa44ca430dac627313759fcca17
Robert Title <rtitle@broadinstitute.org>
2021-04-15 11:55:50 -0400

    Increase even more

--- helm-go-lib/main.go
+++ helm-go-lib/main.go
@@ -206,9 +206,9 @@ func (f *CustomConfigFlags) ToRESTConfig() (*rest.Config, error) {
 	}
 
 	// default is 10/5
-	c.Burst = 100
-	c.QPS = 50
-	log.Printf("Helm client: setting QPS to %d and Burst to %d\n", c.Burst, c.QPS)
+	c.Burst = 1000
+	c.QPS = 100
+	log.Printf("Helm client: setting QPS to %f and Burst to %d\n", c.QPS, c.Burst)
 
 	return c, nil
 }
